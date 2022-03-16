b585b805dd5ad4635d89a95821d87cab99f0dfdb
Aayush Kumar Srivastava <dhpdgwmqo98oafdo@gmail.com>
2022-03-16 12:08:25 +0530

    initial project

---
+++ src/main/java/com/aks/projectmockito/ProjectMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.aks.projectmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aks/projectmockito/ProjectMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aks.projectmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
